85fac905e974671f80a2ae4f044a586dcb1ec924
Strider White <strider@striderwhite.com>
2025-04-12 12:29:50 -0600

    Added ServiceProvider

---
+++ src/EncryptableServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace StriderWhite;
+
+use Illuminate\Support\ServiceProvider;
+
+class EncryptableServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
